1abdf9997b23e34fffa1922e1ad8c7fc329ad631
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2015-03-05 11:17:36 +0100

    Added argparser and multiple cli args - WIP

--- corpustester/FE-test/test_fe_controller.py
+++ corpustester/FE-test/test_fe_controller.py
@@ -4,6 +4,7 @@ import sys
 import codecs
 import string
 import random
+import argparse
 
 from unidecode import unidecode
 
@@ -15,17 +16,14 @@ import json
 #http://localhost:8090/gsibot/bottle/TalkToBot?userAgent=web_html_v1&bot=Duke&type=json&user=UoUinQKS8YYlFAchLu9R&q=eres+el+profesor%3F&undefined=
 url_format = "{base}/TalkToBot?userAgent=web_html_v1&bot=Duke&type=json&user={user}&q={query}&undefined="
 
-def get_test_phrases():
+def get_test_phrases(corpus):
     """
     Read the corpus file, and returns a list with all the phrases
     """
-    corpus = codecs.open('test_corpus.txt', 'r', 'utf-8')
+    corpus = codecs.open(corpus, 'r', 'utf-8')
     lines = [line.replace("\n", "") for line in corpus]
     return lines
 
-def random_agent():
-    return ''.join(random.choice(string.ascii_letters) for _ in range(10))
-    
 def format_url(line, url, agent):
     
     # I am sure there is a nicer way for this, but...
@@ -35,7 +33,7 @@ def format_url(line, url, agent):
     
     return request
     
-def check_response(response, concept):
+def check_response(response, concept, verbose):
     """
     Loads the json response and checks if it gets a valid response
     """
@@ -45,6 +43,9 @@ def check_response(response, concept):
         # Dirty check. If the response is not "Hey, sorry. What were we talking about?"
         # we consider it valid. For the time being.
         #print "q: {query},\n response: {response}".format(query=response_data['dialog']['q'], response=response_data['dialog']['response'])
+        
+        if verbose:
+            print('Bot response: {response}'.format(response_data['dialog']['response']))
         
         # We have a concept to check
         if concept:
@@ -70,26 +71,36 @@ def check_response(response, concept):
     
     return False
 
-def main(url):
+def main(args):
+    
+    if args.verbose:
+        print('Using agent: {agent}'.format(agent=args.agent))
 
-    agent = random_agent()
-    lines =  get_test_phrases()
+    lines =  get_test_phrases(args.corpus)
     
     valid = 0
     notvalid = 0
     
     for line in lines:
         datos = line.split(",") 
-        request = format_url(datos[0], url, agent)
+        request = format_url(datos[0], args.url, args.agent)
+        
+        if args.verbose:
+            print('------------------------------------------------')
+            print("Request URL: {url}".format(url=request))
         
         # Perform the request
         response = urllib.urlopen(request).read()
         
+        if args.verbose:
+            print('Question: {question}'.format(question=datos[0]))
+            if len(datos) > 1:
+                print('Concept: {concept}'.format(concept=datos[1]))
         concept = None
         # Adds
         if len(datos) > 1:
             concept = datos[1]
-        if check_response(response, concept):
+        if check_response(response, concept, args.verbose):
             valid += 1
         else:
             notvalid += 1
@@ -99,7 +110,12 @@ def main(url):
 if __name__ == "__main__":
 
     url = "http://localhost:8090"
-    if len(sys.argv) == 2:
-        url = sys.argv[1]
-
-    main(url)
+    parser = argparse.ArgumentParser(description="Tester for the calista-bot front-end, using a provided corpus", add_help=True)
+    parser.add_argument('-u', '--url', default="http://localhost:8090", help="URL of the talkbot controller")
+    parser.add_argument('-c', '--corpus', default="test_corpus.txt", help="CSV with the questions to test")
+    parser.add_argument('-a', '--agent', default="TestAgent", help="User to use with the bot")
+    parser.add_argument('-v', '--verbose', action='count', help="Print debug info")
+    args = parser.parse_args()
+    print args.url
+    main(args)
+    
