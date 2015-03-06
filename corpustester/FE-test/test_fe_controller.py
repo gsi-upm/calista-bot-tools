53224faec6b45cfa4e03580068dadc3b8ce5041c
Alberto Mardomingo <alberto.mardomingo@gmail.com>
2015-03-06 13:10:27 +0100

    Changed verbosity behaviour

--- corpustester/FE-test/test_fe_controller.py
+++ corpustester/FE-test/test_fe_controller.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 import sys
 import codecs
 import string
@@ -36,64 +38,68 @@ def format_url(line, url, agent):
 def check_response(response, concept, verbose):
     """
     Loads the json response and checks if it gets a valid response
+    Returns a tuple, with wether the result is valid and the actual response
+    (valid, response)
     """
     try:
         response_data = json.loads(response)
-    
+        dialog = response_data[u'dialog']
+        
         # Dirty check. If the response is not "Hey, sorry. What were we talking about?"
         # we consider it valid. For the time being.
         #print "q: {query},\n response: {response}".format(query=response_data['dialog']['q'], response=response_data['dialog']['response'])
         
-        if verbose:
-            print(u'Bot response: {response}'.format(response_data['dialog']['response']))
+        if verbose > 3:
+            print(u'Bot response: {response}'.format(response=dialog[u'response']))
         
         # We have a concept to check
         if concept:
-            if u'topic' not in response_data['dialog']:
+            if u'topic' not in dialog:
                 # Bad response
-                return False
+                return (False, dialog[u'response'])
             
             # We have a topic and a response:
-            if concept in response_data['dialog'][u'topic']:
-                return True
+            if concept in dialog[u'topic']:
+                return (True, dialog[u'response'])
 
-        if u"Lo siento," in response_data['dialog']['response']:
-            return False
+        if "Lo siento," in dialog[u'response']:
+            return (False, dialog[u'response'])
         else:
             # I don't have a topic, but neither a "default" response, so probably a
             # generic "i'm not the teacher" response.
-            return True
+            return (True, dialog[u'response'])
         
     except Exception as e:
         # No json received. Invalid response.
-        print u"Invalid response: "+ response
-        if verbose:
-            print e
+        if verbose >3:
+            print(u"Invalid response: "+ response, file=sys.stderr)
+            print(e, file=sys.stderr)
     
     return False
 
 def main(args):
     
-    if args.verbose:
+    if args.verbose > 1:
         print(u'Using agent: {agent}'.format(agent=args.agent))
 
     lines =  get_test_phrases(args.corpus)
-    
-    valid = 0
-    notvalid = 0
-    
-    for line in lines:
+
+    valid_responses = []
+    invalid_responses = []
+
+    # Ignore the header
+    for line in lines[1:]:
         datos = line.split(",") 
         request = format_url(datos[0], args.url, args.agent)
         
-        if args.verbose:
+        if args.verbose > 3:
             print(u'------------------------------------------------')
             print(u"Request URL: {url}".format(url=request))
         
         # Perform the request
         response = urllib.urlopen(request).read()
         
-        if args.verbose:
+        if args.verbose > 3:
             print(u'Question: {question}'.format(question=datos[0]))
             if len(datos) > 1:
                 print(u'Concept: {concept}'.format(concept=datos[1]))
@@ -101,12 +107,32 @@ def main(args):
         # Adds
         if len(datos) > 1:
             concept = datos[1]
-        if check_response(response, concept, args.verbose):
-            valid += 1
-        else:
-            notvalid += 1
             
-    print u"Resultado: {valid} Validos, {invalid} No validos".format(valid=str(valid), invalid=str(notvalid))
+        q_result = check_response(response, concept, args.verbose)
+        if q_result[0]:
+            valid_responses.append((datos[0],q_result[1]))
+        else:
+            invalid_responses.append((datos[0],q_result[1]))
+    
+    # Print wrong results with a lower verbosity level
+    if args.verbose >2:
+        print(u'------------------------------------------------------')
+        print(u"Valid results:")
+        for res in valid_responses:
+            print(u"Q: {q}, R: {r}".format(q=res[0], r=res[1]))
+        print(u'------------------------------------------------------')
+    
+    if args.verbose >1:
+        print(u'------------------------------------------------------')
+        print(u"Valid results:")
+        for res in invalid_responses:
+            print(u"Q: {q}, R: {r}".format(q=res[0], r=res[1]))
+        print(u'------------------------------------------------------')
+    
+    # Always print final result
+    print(u"Resultado: {valid} Validos, {invalid} No validos".format(valid=str(len(valid_responses)),
+                                                                     invalid=str(len(invalid_responses))))
+    
 
 if __name__ == "__main__":
 
